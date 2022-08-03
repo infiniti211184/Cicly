e2ef3d1892f6b9614abe5470dca083c852c12a18
Julia Khramova <infiniti211184@mail.ru>
2022-08-03 21:03:32 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+    // задание 1
+public class Main {
+    public static void main(String[] args) {
+        int i = 1;
+         wile (i <=10) {
+             1++
+        System.out.println("%d", i );
+    }
+}
+
+    // задание 2
+    int firstFridayOfMonth = 5;
+    for (;firstFridayOfMonth <=31; FirstFrideyOfMonth +=7) {
+        System.out.printf("сегодня пятница, %d-е число, необходимо подготовить отчет.%n", firstFridayOfMonth);
+        }
+
+
+    // задание 3
+    int period = 79;
+    int currentYear = 2022;
+    int start - currentYear-200;
+    int end - currentYear+100;
+    int startYear = 0;
+    for (int year = startYear; year<=end: year++) {
+       if (year% period == startYear)}
+
+        public int getFirstFridayOfMonth() {
+            return firstFridayOfMonth;
+        }
+    System.out.println(year);
+
+
